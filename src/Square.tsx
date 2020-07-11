e97f7404a2498e28d22b586aca2a25b365cdef4a
Matti Sangi <adm.masaweb@gmail.com>
2020-07-11 11:05:26 +0300

    Add state object to class Square

--- src/Square.tsx
+++ src/Square.tsx
@@ -3,11 +3,20 @@ import './Square.css'
 
 export interface SquareProps { value: number; }
 
-export class Square extends React.Component<SquareProps, {}> {
+export interface SquareState { value: string | null; }
+
+export class Square extends React.Component<SquareProps, SquareState> {
+    constructor(props: SquareProps) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <button className="square">
-                {this.props.value}
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
             </button>
         )
     }
